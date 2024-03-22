bd7c93bbab89497ddc01224161b0e0d707a183af
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-03-22 13:48:02 +0200

    Lab 5

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,26 +10,16 @@ use Illuminate\Support\Facades\Storage;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $tasks = Task::with('user')->paginate(10);
         return view('tasks.index', compact('tasks'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('tasks.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
 
     public function store(StoreTaskRequest $request)
     {
@@ -45,29 +35,18 @@ class TaskController extends Controller
         return redirect()->route('tasks.index');
     }
 
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-
         $task = Task::findOrFail($id);
         return view('tasks.show', compact('task'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         $task = Task::findOrFail($id);
         return view('tasks.edit', compact('task'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateTaskRequest $request, string $id)
     {
         if ($request->hasFile('image')) {
@@ -88,9 +67,6 @@ class TaskController extends Controller
         return redirect()->route('tasks.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         $task = Task::findOrFail($id);
